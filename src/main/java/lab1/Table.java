a880ca650bc77d5265cfd3cb231af944481eeb34
les0327 <les.volodumur@gmail.com>
2017-09-17 22:45:09 +0300

    Initial commit

---
+++ src/main/java/lab1/Table.java
@@ -0,0 +1,200 @@
+package lab1;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+
+public class Table<DK extends Comparable, FK extends CharSequence, FF> {
+
+    @Getter private ArrayList<TableRow> rows;
+    @Getter private boolean isSorted;
+
+    public Table () {
+        this.rows     = new ArrayList<>();
+        this.isSorted = true;
+    }
+
+    /**
+     *  Add new row to the end
+     * @param directAddressKey  -
+     * @param foreignAddressKey -
+     * @param functionalField   -
+     */
+    public void add(DK directAddressKey, FK foreignAddressKey, FF functionalField) {
+        this.rows.add(new TableRow(directAddressKey, foreignAddressKey, functionalField));
+        this.isSorted = false;
+    }
+
+    /**
+     * Add new row to the end
+     * @param row - row to add
+     */
+    public void add(TableRow row) {
+        this.rows.add(row);
+        this.isSorted = false;
+    }
+
+    /**
+     * Insert new row in index position
+     * @param index             -
+     * @param directAddressKey  -
+     * @param foreignAddressKey -
+     * @param functionalField   -
+     */
+    public void insert(int index, DK directAddressKey, FK foreignAddressKey, FF functionalField) {
+        this.rows.add(index, new TableRow(directAddressKey, foreignAddressKey, functionalField));
+        this.isSorted = false;
+    }
+
+    /**
+     * Insert new row in index position
+     * @param index -
+     * @param row   -
+     */
+    public void insert(int index, TableRow row) {
+        this.rows.add(index, row);
+        this.isSorted = false;
+    }
+
+    /**
+     * Select row with i index from table
+     * @param i - index
+     * @return row with i index or null if index > table size
+     */
+    public TableRow get(int i) {
+        if (i < rows.size())
+            return rows.get(i);
+        else
+            return null;
+    }
+
+    /**
+     * Select row with i index from table
+     * @param directKey - direct address key
+     * @return row with i index or null if table is empty
+     */
+    public TableRow select(DK directKey) {
+        if (!rows.isEmpty())
+            if (isSorted)
+                return binarySearch(directKey);
+            else
+                return linearSearch(directKey);
+        else
+            return null;
+    }
+
+    /**
+     * Update table row if table contain it
+     * @param directAddressKey  -
+     * @param foreignAddressKey -
+     * @param functionalField   -
+     */
+    public void update(DK directAddressKey, FK foreignAddressKey, FF functionalField) {
+        TableRow oldRow = this.select(directAddressKey);
+        if (oldRow != null) {
+            oldRow.setForeignAddressKey(foreignAddressKey);
+            oldRow.setFunctionalField(functionalField);
+        }
+    }
+
+    /**
+     * Delete row with directAddressKey
+     * @param directAddressKey -
+     */
+    public void delete(DK directAddressKey) {
+        rows.remove(this.select(directAddressKey));
+    }
+
+
+    public class TableRow {
+
+        @Getter @Setter private DK directAddressKey;
+        @Getter @Setter private FK foreignAddressKey;
+        @Getter @Setter private FF functionalField;
+
+        public TableRow(DK directAddressKey, FK foreignAddressKey, FF functionalField) {
+            this.directAddressKey  = directAddressKey;
+            this.foreignAddressKey = foreignAddressKey;
+            this.functionalField   = functionalField;
+        }
+
+        @Override
+        public String toString() {
+            return "TableRow{" +
+                    "directAddressKey=" + directAddressKey +
+                    ", foreignAddressKey=" + foreignAddressKey +
+                    ", functionalField=" + functionalField +
+                    '}';
+        }
+    }
+
+    /**
+     *  Linear search
+     * @param directKey - direct address key
+     * @return first TableRow with direct address key == directKey or null if nothing found
+     */
+    private TableRow linearSearch(DK directKey) {
+        Iterator<TableRow> itr = rows.iterator();
+
+        do {
+            TableRow buf   = itr.next();
+            if (buf.getDirectAddressKey().equals(directKey))
+                return buf;
+        } while (itr.hasNext());
+
+        return null;
+    }
+
+    /**
+     * Binary search
+     * @param directKey - direct address key
+     * @return TableRow with direct address key == directKey or null if nothing found
+     */
+    private TableRow binarySearch(DK directKey) {
+
+        int left  = 0;
+        int right    = rows.size() - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            DK buf = rows.get(mid).getDirectAddressKey();
+            if (buf.compareTo(directKey) > 0 )
+                right = mid - 1;
+            else if (buf.compareTo(directKey) < 0)
+                left = mid + 1;
+            else
+                return rows.get(mid);
+        }
+
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer buf = new StringBuffer("Table{\n");
+        rows.forEach(row -> buf.append('\t').append(row.toString()).append('\n'));
+        return  buf.append("};").toString();
+    }
+
+    public void sort(Comparator<TableRow> comparator) {
+        rows.sort(comparator);
+        isSorted = true;
+    }
+
+    /**
+     * Clear table
+     */
+    public void clear() {
+        rows.clear();
+    }
+
+    /**
+     * @return table's size
+     */
+    public long getSize() {
+        return rows.size();
+    }
+}
---
+++ src/test/java/lab1/TableTest.java
@@ -0,0 +1,72 @@
+package lab1;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TableTest {
+
+    private static Table<Long, String, Float> table = new Table<>();
+
+    @Before
+    public void setUpTable() {
+        table.add((long) 6, "lKtFm", 9.103f);
+        table.add((long) 3, "cdefg", 5.7f);
+        table.add((long) 1, "abCde", 2.0f);
+        table.add((long) 2, "bcdeF", 3.3f);
+        table.add((long) 4, "ggeEf", 6.66f);
+    }
+
+    @After
+    public void tearDownTable() {
+        table.clear();
+    }
+
+    @Test
+    public void add() throws Exception {
+        System.out.printf("Table before add:%n%s%n", table.toString());
+        table.add((long) 7, "FFOLk", 11.9f);
+        System.out.printf("Table after add:%n%s%n", table.toString());
+    }
+
+    @Test
+    public void insert() throws Exception {
+        System.out.printf("Table before insert:%n%s%n", table.toString());
+        table.insert(0,(long) 7, "FFOLk", 11.9f);
+        System.out.printf("Table after insert:%n%s%n", table.toString());
+    }
+
+    @Test
+    public void get() throws Exception {
+        System.out.println(table);
+        System.out.printf("get(%d) : %s%n", 0, table.get(0).toString());
+        System.out.printf("get(%d) : %s%n", 3, table.get(3).toString());
+    }
+
+    @Test
+    public void select() throws Exception {
+        System.out.println(table);
+        System.out.printf("Select(%d)(linear) : %s%n", 2,     table.select((long) 2));
+        System.out.printf("Select(%d)(linear) : %s%n", Long.MAX_VALUE, table.select(Long.MAX_VALUE));
+
+        table.sort((row1, row2) -> row1.getDirectAddressKey().compareTo(row2.getDirectAddressKey()));
+        System.out.printf("Sorted %s%n", table.toString());
+        System.out.printf("Select(%d)(binary) : %s%n", 2,     table.select((long) 2));
+        System.out.printf("Select(%d)(binary) : %s%n", Long.MAX_VALUE, table.select(Long.MAX_VALUE));
+    }
+
+    @Test
+    public void update() throws Exception {
+        System.out.printf("Before update : %n%s%n", table);
+        table.update((long) 6, "UPDATED", Float.MAX_VALUE);
+        System.out.printf("After update : %n%s%n", table);
+    }
+
+    @Test
+    public void delete() throws Exception {
+        System.out.printf("Before delete : %n%s%n", table);
+        table.delete((long) 6);
+        table.delete((long) 2);
+        System.out.printf("After delete : %n%s%n", table);
+    }
+}
